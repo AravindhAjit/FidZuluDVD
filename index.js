5ce2e382d16e745686c203fc140adc44a814afd7
AravindhAjit <80188492+AravindhAjit@users.noreply.github.com>
2022-11-07 13:43:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const router = express.Router();
+const createError = require('http-errors');
+const DVDsjson = require('../module/dvd');
+const url = require('url');
+
+function computePrice(json, percent) {
+  percent = percent / 100;
+  for (let dvd of json) {
+      console.log(dvd.price);
+      dvd.price = dvd.price * (1 + percent)
+      console.log(dvd.price);
+  }
+  console.log(json);
+  return json
+}
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'BackendTeamB' });
+});
+
+router.get('/DVDs/all/:location', (req, res, next) => {
+
+  const param = req.params.location;
+  console.log('got into dvd/all/:location ' + param);
+
+  const result = DVDsjson.list();
+  if (result) {
+    if (param.toLowerCase() === "durham") {
+      result.forEach(element => {
+        element.price = (element.price * 1.08).toFixed(2)
+      });
+    }
+    else if (param.toLowerCase() === "raleigh"){
+      result.forEach(element => {
+        element.price = (element.price * 1.075).toFixed(2)
+      });
+    } else{
+      next(createError(404));
+    }
+    res.setHeader('content-type', 'application/json');
+    res.end(JSON.stringify(result));
+  } else {
+    next(createError(404));
+  }
+});
+
+
+
+router.get('/DVDs/team', (req, res, next) => {
+  let result = {"team": "DVD", "membersNames":["Aravindhan", "Pooja", "Pratik","Nandana"]};
+  res.setHeader('content-type', 'application/json');
+  res.end(JSON.stringify(result));
+})
+
+
+module.exports = router;
